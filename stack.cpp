d1b3da97d643f8ea1a82ca885adf9cae45cf5989
ted523 <tedotieno523@gmail.com>
2021-03-08 10:11:13 +0300

    Functions Implemented

--- stack.cpp
+++ stack.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// implementation of stack data structure that stores INTEGER elements 
 class Stack{
   private:
     int top;
@@ -43,12 +44,44 @@ class Stack{
 		}
 		else
 		{
-			top++;
-			arr[top]=val;
+			top++;// Assuming the stack has no value in it, and initially the index top is assigned [-1], then this increment operation should move us to index [0] 
+			arr[top]=val; // At the current index, assign the value passed as argument
 		}
 	}
 	
+	int pop()
+	{
+		if(isEmpty())
+		{
+			cout<<"Stack Underflow"<<endl;
+			return 0;
+		}
+		else
+		{
+			int popvalue = arr[top];
+			arr[top] = 0;
+			top--;
+			return popvalue;
+		}
+	}
 	
+	int count()
+	{
+		return (top+1); // Returns the number of values in the stack
+	}
+	
+	int peek (int pos)
+	{
+		if (isEmpty())
+		{
+			cout<<"Stack Underflow"<<endl;
+			return 0;
+		}
+		else
+		{
+			return arr[pos];// The argument being passed in this function is the index of the value which we would like to view in the stack
+		}
+	}
 
 
 };
